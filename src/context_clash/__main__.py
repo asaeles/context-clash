b1d4687d5dc161a9ba3df316970bff73e30efee2
Ahmad Sulaeman <asaeles@gmail.com>
2026-08-22 01:37:16 +0300

    Docker+Package

--- src/context_clash/__main__.py
+++ src/context_clash/__main__.py
@@ -1,13 +1,4 @@
-import os
-import sys
-from streamlit.web import cli as stcli
-
-def main():
-    # This points to the app.py in the same folder
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, "app.py")
-    sys.argv = ["streamlit", "run", filename]
-    sys.exit(stcli.main())
+from .cli import main
 
 if __name__ == "__main__":
     main()
